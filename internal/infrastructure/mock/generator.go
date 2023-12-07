4a33b606349ebce81c09f46e9b192eca1846617e
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-12-08 01:45:30 +0900

    create generator

---
+++ internal/infrastructure/mock/generator.go
@@ -0,0 +1,58 @@
+package mock
+
+import (
+	"context"
+	"math/rand"
+	"time"
+)
+
+
+const defaultPrice = 1000
+
+type generator struct {
+	symbol string
+
+	curTime  time.Time
+	curPrice float64
+}
+
+func newGenerator(symbol string, d time.Duration) *generator {
+	return &generator{
+		symbol: symbol,
+		curTime:  time.Now(),
+		curPrice: defaultPrice,
+	}
+}
+
+func (m *generator) Next() *Trade {
+
+	curTime := time.Now()
+	curPrice := m.curPrice * (rand.Float64()*0.2 + 0.9)
+
+	trade := &Trade{
+		Symbol:   m.symbol,
+		Price:    curPrice,
+		Amount:   1,
+		Timestamp: curTime,
+	}
+
+	m.curTime = curTime
+	m.curPrice = curPrice
+
+	return trade
+}
+
+func RunGenerator(ctx context.Context, id string, d time.Duration, ch chan<- *Trade) {
+
+	g := newGenerator(id, d)
+	ch <- g.Next()
+
+	ticker := time.NewTicker(d)
+
+	for range ticker.C {
+		if ctx.Err() != nil {
+			return
+		}
+		ch <- g.Next()
+	}
+}
---
+++ internal/infrastructure/mock/generator_test.go
@@ -0,0 +1,38 @@
+package mock_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/Goboolean/fetch-system.worker/internal/infrastructure/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+
+
+func TestRunGenerator(t *testing.T) {
+	
+	const (
+		symbol = "MOCK"
+		duration = time.Millisecond * 1
+
+		count = 1000
+		deadline = time.Second * 1
+	)
+
+	ch := make(chan *mock.Trade, count * 2)
+
+	ctx, cancel := context.WithTimeout(context.Background(), deadline)
+	defer cancel()
+
+	// runs until the deadline
+	go mock.RunGenerator(ctx, symbol, duration, ch)
+
+	time.Sleep(deadline)
+	time.Sleep(time.Millisecond * 10)
+
+	// assures 99% accuracy 
+	assert.LessOrEqual(t, int(count * 0.99), len(ch))
+	assert.GreaterOrEqual(t, int(count * 1.01), len(ch))
+}
