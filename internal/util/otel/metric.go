d0767693ebc57d8d147614e759df019c1b5b6c0e
mulmuri <68294539+mulmuri@users.noreply.github.com>
2024-01-22 13:33:54 +0900

    commits of current job

--- internal/util/otel/metric.go
+++ internal/util/otel/metric.go
@@ -1,6 +1,8 @@
 package otel
 
 import (
+	"context"
+
 	"go.opentelemetry.io/otel/metric"
 )
 
@@ -26,51 +28,61 @@ func initMetric(meter metric.Meter) (err error) {
 	if err != nil {
 		return
 	}
+	KafkaProducerErrorCount.Add(context.Background(), 0)
 
 	KafkaProducerErrorCount, err = meter.Int64Counter("kafka.producer.error.count")
 	if err != nil {
 		return
 	}
+	KafkaProducerErrorCount.Add(context.Background(), 0)
 
 	ETCDErrorCount, err = meter.Int64Counter("etcd.error.count")
 	if err != nil {
 		return
 	}
+	ETCDErrorCount.Add(context.Background(), 0)
 
 	MockGeneratorCount, err = meter.Int64Counter("mock.generator.count")
 	if err != nil {
 		return
 	}
+	MockGeneratorCount.Add(context.Background(), 0)
 
 	PolygonStockErrorCount, err = meter.Int64Counter("polygon.stock.error.count")
 	if err != nil {
 		return
 	}
+	PolygonStockErrorCount.Add(context.Background(), 0)
 
 	PolygonStockReceivedCount, err = meter.Int64Counter("polygon.stock.received.count")
 	if err != nil {
 		return
 	}
+	PolygonStockReceivedCount.Add(context.Background(), 0)
 
 	KISStockErrorCount, err = meter.Int64Counter("kis.stock.error.count")
 	if err != nil {
 		return
 	}
+	KISStockErrorCount.Add(context.Background(), 0)
 	
 	KISStockReceivedCount, err = meter.Int64Counter("kis.stock.received.count")
 	if err != nil {
 		return
 	}
+	KISStockReceivedCount.Add(context.Background(), 0)
 
 	ProductSubscribedCount, err = meter.Int64Counter("product.subscribed.count")
 	if err != nil {
 		return
 	}
+	ProductSubscribedCount.Add(context.Background(), 0)
 
 	ProductReceivedCount, err = meter.Int64Counter("product.received.count")
 	if err != nil {
 		return
 	}
+	ProductReceivedCount.Add(context.Background(), 0)
 
 	return
 }
